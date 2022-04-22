a69bc5d17f56a13905f54ad67e8777d8ef91606d
MazzGuille <mazzcode@gmail.com>
2022-04-22 15:36:40 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("What is your name?");
+string name = Console.ReadLine();
+Console.WriteLine("Hello, " + name + " on " + DateTime.Now + "!");
+string surename = "mazzari";
+Console.WriteLine(name + " " + surename);
+
+Console.WriteLine("What is your name?");
+string name2 = Console.ReadLine();
+Console.WriteLine("What is your surename?");
+string surename2 = Console.ReadLine();
+Console.WriteLine("What is your age?");
+string age = Console.ReadLine();
+Console.WriteLine("You are " + name2 + " you are " + age + " years old and your last name is " + surename2);
+
